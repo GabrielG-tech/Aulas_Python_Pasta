ba9df25671283bd1f459a474a59c6f21c4f78d89
Gabriel <gabriel@mpsouza.com.br>
2022-05-31 08:41:26 -0300

    Aulas python 1.6.0

--- 06-Arqvs/Exercicios/f1/main.py
+++ 06-Arqvs/Exercicios/f1/main.py
@@ -11,7 +11,6 @@ drivers = tratar_dados_do_arquivo('06-Arqvs\Exercicios\\f1\\00drivers.csv')
 races = tratar_dados_do_arquivo('06-Arqvs\Exercicios\\f1\\02races.csv')
 results = tratar_dados_do_arquivo('06-Arqvs\Exercicios\\f1\\03results.csv')
 
-# Retornar todas as corridas de uma temporada
 def pegar_corridas_de_temporada(ano):
     corridas_temporada = []
     for race in races:
@@ -19,7 +18,6 @@ def pegar_corridas_de_temporada(ano):
             corridas_temporada.append(race)
     return corridas_temporada
 
-# Retornar os resultados de um piloto em uma temporada
 '''
 def pegar_id_do_piloto(nome):
     pilotos = []
@@ -48,21 +46,41 @@ def pegar_ids_das_corridas_da_temporada(ano):
 def pegar_resultados_do_piloto_na_temporada(nome, sobrenome, ano):
     resultados = []
     id_piloto = pegar_id_do_piloto(nome, sobrenome)         # id vazio
-    ids_corridas = pegar_ids_das_corridas_da_temporada(ano) # lista vazia
+    ids_corridas = pegar_ids_das_corridas_da_temporada(ano) # lista 
+    # print(id_piloto)
+    # print(ids_corridas)
     for resultado in results:
         if (resultado[1] in ids_corridas and resultado[2] == id_piloto):
             resultados.append(resultado)
     return resultados
 
-
-# Retornar a pontuação total de um piloto em cada temporada
-
-def app():
-    # print(pegar_id_do_piloto('Jean', 'Alesi'))
-    # print(pegar_id_do_piloto('Nelson', 'Piquet'))
-    resultados = pegar_resultados_do_piloto_na_temporada('Nico', 'Rosberg', 2009)
-    print("Posição\tPontos")
+def exibir_pontuacao_piloto_na_temporada(nome, sobrenome, ano):
+    total_pontos = 0
+    resultados = pegar_resultados_do_piloto_na_temporada(nome, sobrenome, ano)
     for resultado in resultados:
-        print(f"{resultado[6]}\t{resultado[9]}")
+        total_pontos += float(resultado[9])
+    return f"{nome} {sobrenome} \t Temporada {ano}: \t {total_pontos}"
+
+def pegar_todas_temporadas():
+    temporadas = []
+    for corrida in races:
+        ano = corrida[1]
+        if not ano in temporadas:
+            temporadas.append(ano)
+    return temporadas
+
+def exibir_pontuacoes_do_piloto(nome, sobrenome):
+    for ano in pegar_todas_temporadas():
+        print(exibir_pontuacao_piloto_na_temporada(nome, sobrenome, ano))
+
+# Menu de interação
+def app():
+    exibir_pontuacoes_do_piloto('Nico', 'Rosberg')
+    # Retornar todas as corridas de uma temporada
+    # Retornar os resultados de um piloto em uma temporada
+    # Retornar a pontuação total de um piloto em cada temporada
+    # Itens extras
+    # Buscar informações de um piloto buscando pelo seu nome ou sobrenome
+    # Buscar informações de um circuito pelo seu país
 
 app()

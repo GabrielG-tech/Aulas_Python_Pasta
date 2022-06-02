05285630d63097add8c1a8a70a2bc7f51cad0373
Gabriel Gomes <gabriel@mpsouza.com.br>
2022-06-02 09:19:19 -0300

    TP04_1.1.1

--- LCA - TP04/Questão1.py
+++ LCA - TP04/Questão1.py
@@ -6,23 +6,13 @@ def clear():
 clear()
 
 valor_total = float(input('Informe o valor total do consumo: R$'))
-num_pessoas = input('Informe o total de pessoas: ')
+num_pessoas = int(input('Informe o total de pessoas: '))
 perc_serv = float(input('Informe o percentual do serviço, entre 0 e 100: '))
-
-def verificação():
-    while num_pessoas >= 0 or num_pessoas != int:
-        num_pessoas = input('Valor inválido\nPorfavor insira outro valor: ')
-    while valor_total >= 0:
-        valor_total = input('Valor inválido\nPorfavor insira outro valor: ')
-    while 0 < perc_serv < 100:
-        valor_total = input('Valor inválido\nPorfavor insira outro valor: ')
-        
-#def dividir_conta(): 
-#    valor_dividido = valor_final / num_pessoas
+perc_serv = perc_serv/100
 
 
 valor_final = valor_total + (valor_total * perc_serv)
-
+valor_dividido = valor_final / num_pessoas
 
 valor_final = str(valor_final)     # Converte o valor para uma string
 valor_final.replace('.', ',')      # Substitui pontos por vírgulas
@@ -33,3 +23,7 @@ valor_final.replace('.', ',')      # Substitui pontos por vírgulas
 
 print(f'O valor total da conta, com a taxa de serviço, será de R${valor_dividido}')
 print(f'Dividindo a conta por {num_pessoas} pessoa(s), cada pessoa deverá pagar R${valor_final}.')
+
+# O programa deve verificar se o número total de pessoas é maior do que zero.
+# O programa deve verificar se o percentual do serviço está dentro do intervalo válido, de 0 a 100. 
+# Caso valores inválidos sejam digitados, deve ser exibida a mensagem de erro “Valor inválido” e o programa deve ser interrompido.

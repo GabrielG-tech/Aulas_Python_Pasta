23693fdd917c3c874c7260c5443be291e92e5202
GabrielG-tech <gabriel@mpsouza.com.br>
2022-06-14 08:34:55 -0300

    Pandas

---
+++ 07-Pandas/main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# print(pd.__version__)
+
+drivers_df = pd.read_csv('07-Pandas\\00drivers.csv'), index_col=
+
+# print(drivers_df)
+# print(drivers_df.head(11))
+# print(drivers_df.tail(11))
+
+

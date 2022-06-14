a5340aa8ce35c8a34533847ae9057e4db200d627
GabrielG-tech <gabriel@mpsouza.com.br>
2022-06-14 09:32:50 -0300

    03

--- 07-Pandas/main.py
+++ 07-Pandas/main.py
@@ -2,10 +2,19 @@ import pandas as pd
 
 # print(pd.__version__)
 
-drivers_df = pd.read_csv('07-Pandas\\00drivers.csv'), index_col=
+drivers_df = pd.read_csv('07-Pandas\\00drivers.csv') #, index_col=0)
 
 # print(drivers_df)
-# print(drivers_df.head(11))
-# print(drivers_df.tail(11))
+# print(drivers_df.head(10))
+# print(drivers_df.tail())
+# print(drivers_df.columns)
+# drivers_df = drivers_df.sort_index(axis=1, ascending=True)
+# axis=0 (ordena por linhas) ou axis=1 (ordena por colunas)
+# ascending=True (Ordena de maneira crescente) ou ascending=False (Ordena de maneira decrescente)
+# drivers_df = drivers_df.sort_values(by=["surname", "forename"])
+#   print(drivers_df([["forename", "surname"]]).head(10))
+# print(drivers_df[10:20])
 
+#  print(drivers_df.iloc[0, :])
 
+# print(drivers_df.loc[drivers_df['driverId']])

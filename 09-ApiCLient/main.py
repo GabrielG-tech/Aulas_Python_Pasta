f2bac8118841aee3b918fff4e1a62b6d33791a02
Gabriel Gomes <gabriel@mpsouza.com.br>
2022-06-21 10:12:17 -0300

    joguinho_top

--- 09-ApiCLient/main.py
+++ 09-ApiCLient/main.py
@@ -16,6 +16,16 @@ headers = {
 }
 
 field = 'LCA22T2M1'
-game = requests.get(api_url + "/games/" + field + 'add/me', headers=headers)
+# game = requests.get(api_url + "/games/" + field + '/add/me', headers=headers)
 #game = requests.post(api_url + "/games", headers=headers, json = {'field': field})
-print(game.json())
+
+# action = requests.get(api_url + "/games/" + field + "/move/" + 'n', headers=headers)
+action = requests.get(api_url + "/games/" + field + "/attack/" + 'n', headers=headers)
+
+#       N
+#   NW  |  NE
+# W ----+---- E
+#   SW  |  SE
+#       S
+
+print(action.json())

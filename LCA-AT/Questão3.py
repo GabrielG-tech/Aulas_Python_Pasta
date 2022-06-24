67c66dc3c3d97b4ef6d36df39a671eabccc52447
Gabriel <gabriel@mpsouza.com.br>
2022-06-23 22:32:29 -0300

    questão3_AT

--- LCA-AT/Questão3.py
+++ LCA-AT/Questão3.py
@@ -4,44 +4,40 @@
 # informando qual é o percentual da renda mensal total comprometido por cada custo. 
 
 renda_mensal_total = float(input('Renda mensal total: '))
-gasto_moradia = float(input('Gasto com moradia: '))
-gastos_totais_educacao = float(input('Gasto com educação: '))
-gastos_totais_transporte = float(input('Gasto com transporte: '))
+moradia = float(input('Gasto com moradia: '))
+educacao = float(input('Gasto com educação: '))
+transporte = float(input('Gasto com transporte: '))
 
 # Se o gasto estiver dentro do percentual recomendado, informe ainda que:
 # "Seus gastos estão dentro da margem recomendada."
 
 # Caso contrário, informe:
 # "Portanto, idealmente, o máximo de sua renda comprometida com {tipo} deveria ser de R$ {valor_max}"
+
 valor_max_moradia = renda_mensal_total * 0.3
 valor_max_educacao = renda_mensal_total * 0.2
 valor_max_transporte = renda_mensal_total * 0.15
 
-print('valor_max_moradia = {}, valor_max_educacao = {}, valor_max_transporte = {}'.format(valor_max_moradia, valor_max_educacao, valor_max_transporte))
+perc_moradia = moradia/renda_mensal_total * 100
+perc_educacao = educacao/renda_mensal_total * 100
+perc_transporte = transporte/renda_mensal_total * 100
 
+perc_moradia = round(perc_moradia, 1)
+perc_educacao = round(perc_educacao, 1)
+perc_transporte = round(perc_transporte, 1)
 
-#def diagnostico_financeira_usuario():
-#    if gasto_moradia/renda_mensal_total <= 0.3 and gastos_totais_educacao/renda_mensal_total <= 0.2 and gastos_totais_transporte/renda_mensal_total <= 0.15:
-#        print("Seus gastos estão dentro da margem recomendada.")
-#    elif :
-#        print(f"Portanto, idealmente, o máximo de sua renda comprometida com {tipo} deveria ser de R$ {valor_max}")
-
-# Onde "tipo" deve ser moradia, educação ou transportes e "valor_max"
-# deve ser o valor equivalente ao percentual máximo de gasto com esse tipo de custo.
-
-# Exemplo de saída do programa:
-
-# Renda mensal total: R$ 5000
-# Gastos totais com moradia: R$ 1760
-# Gastos totais com educação: R$ 800
-# Gastos totais com transporte: R$ 300
-
-# Diagnóstico:
-# Seus gastos totais com moradia comprometem 35.2% de sua renda total.
-# O máximo recomendado é de 30%. Portanto, idealmente, o máximo de sua renda comprometida com moradia deveria ser de R$ 1500.
-
-# Seus gastos totais com educação comprometem 16% de sua renda total.
-# O máximo recomendado é de 20%. Seus gastos estão dentro da margem recomendada.
-
-# Seus gastos totais com transporte comprometem 6% de sua renda total.
-# O máximo recomendado é de 15%. Seus gastos estão dentro da margem recomendada.
+def diagnostico_financeiro_usuario():
+    print('Diáginostico: ')
+    if perc_moradia <= 30:
+        print(f'Seus gastos totais com moradia comprometem {perc_moradia}% de sua renda total. O máximo recomendado é de 30%. Seus gastos estão dentro da margem recomendada.')
+    else:
+        print(f'Seus gastos totais com moradia comprometem {perc_moradia}% de sua renda total. O máximo recomendado é de 30%. Portanto, idealmente, o máximo de sua renda comprometida com moradia deveria ser de R${valor_max_moradia}')
+    if perc_educacao <= 20:
+        print(f'Seus gastos totais com moradia comprometem {perc_educacao}% de sua renda total. O máximo recomendado é de 20%. Seus gastos estão dentro da margem recomendada.')
+    else:
+        print(f'Seus gastos totais com moradia comprometem {perc_educacao}% de sua renda total. O máximo recomendado é de 20%. Portanto, idealmente, o máximo de sua renda comprometida com educacao deveria ser de R${valor_max_educacao}')
+    if perc_transporte <= 15:
+        print(f'Seus gastos totais com moradia comprometem {perc_transporte}% de sua renda total. O máximo recomendado é de 15%. Seus gastos estão dentro da margem recomendada.')
+    else:
+        print(f'Seus gastos totais com moradia comprometem {perc_transporte}% de sua renda total. O máximo recomendado é de 15%. Portanto, idealmente, o máximo de sua renda comprometida com transporte deveria ser de R${valor_max_transporte}')
+diagnostico_financeiro_usuario()

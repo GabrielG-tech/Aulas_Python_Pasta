2d136a21a355e05408bf51dbc58420989d5ab019
Gabriel <gabriel@mpsouza.com.br>
2022-06-24 01:47:40 -0300

    terminandoAT

---
+++ LCA-AT/Questão5.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+
+
+# 3. O aluno criou a função que exibe o PIB de um país em um ano na questão 5?
+
+# 3. O aluno criou a função que exibe a variação de PIB por país ao longo do período na questão 5?
+
+# 3. O aluno realizou a leitura do arquivo csv na questão 5?
+
+# 3. O aluno extraiu os dados de PIB dos países do arquivo csv na questão 5?
+
+# 3. O código desenvolvido exibiu corretamente o gráfico da evolução do PIB de um país na questão 5?
+
+# 4. O aluno conceitualizou bancos de dados?
+
+# 4. O aluno deu exemplos de uso de bancos de dados em aplicações?
+
+# 3. Falta 9/14
+# 4. Falta 2/6

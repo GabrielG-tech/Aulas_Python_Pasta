b279b2550dfce206fe219d9f1ba63d9439838f36
Gabriel <gabriel@mpsouza.com.br>
2022-06-24 01:51:14 -0300

    AT_2

--- LCA-AT/Questão5.py
+++ LCA-AT/Questão5.py
@@ -1,12 +1,11 @@
 import pandas as pd
 
-
+tabela_pib = pd.read_csv("LCA-AT\Assessment_PIBs - modelo 1.csv")
+print(tabela_pib)
 
 # 3. O aluno criou a função que exibe o PIB de um país em um ano na questão 5?
 
 # 3. O aluno criou a função que exibe a variação de PIB por país ao longo do período na questão 5?
-
-# 3. O aluno realizou a leitura do arquivo csv na questão 5?
 
 # 3. O aluno extraiu os dados de PIB dos países do arquivo csv na questão 5?
 

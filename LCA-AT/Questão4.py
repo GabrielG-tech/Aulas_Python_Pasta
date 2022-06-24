5460eb7f2118f7ac6b8b93253ddacc4d1ffcb952
Gabriel <gabriel@mpsouza.com.br>
2022-06-23 23:47:08 -0300

    questao4_at_feita

--- LCA-AT/Questão4.py
+++ LCA-AT/Questão4.py
@@ -6,50 +6,25 @@
 # tendo como eixo das abscissas (horizontal) o número de períodos e,
 # no eixo das ordenadas (vertical), o valor acumulado, em reais.
 
+import matplotlib.pyplot as plt
 
+def calcular_juros_compostos(valor_inicial, quant_aporte, juros_mensais, periodos):
+    valor_total = valor_inicial
+    valor_total_grafico = []
+    valor_total_periodo = []
+    for item in range(periodos):
+        valor_total += (valor_total * juros_mensais)/100
+        valor_total += quant_aporte 
+        valor_total_grafico.append(round(valor_total))
+        valor_total_periodo.append(item)
+        print(f'Após {item} períodos(s), o montante será de R${round(valor_total, 2)}.')
+    plt.plot(valor_total_periodo, valor_total_grafico)
+    plt.show()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# ========= Dagoberto ============
-
-# import matplotlib.pyplot as plt
-
-# def pegar_dados ():
-#     dados = []  # 0 Valor inicial /  1 Redimento p/periodo / 2 Aporte / 3 Total de periodos
-#     dados.append(float(input("Valor inicial: R$ ")))
-#     dados.append(float(input("Aporte a cada período: R$ ")))
-#     dados.append(float(input("Rendimento por período em %: ")))
-#     dados.append(int(input("Quantidade de períodos: ")))
-#     return dados
-
-# def juros (dados) :
-#      global valor_grafico
-#      global valor_periodo
-#      valor = dados[0]
-#      valor_grafico = []
-#      valor_periodo = []
-#      for i in range(dados[3]):
-#          valor += (valor * dados[1]) / 100
-#          valor += dados[2]
-#          print("Após {} períodos(s), o montante será de R${}".format(i+1, round(valor, 2)))
-#          valor_grafico.append(round(valor))
-#          valor_periodo.append(i)
-#      plt.plot(valor_periodo, valor_grafico)
-#      plt.show()
-
-
-# dados = pegar_dados()
-# juros(dados)
+def aplicar_juros_dados_usuario():
+    valor_inicial = float(input("Valor inicial: R$ "))
+    rendimento_periodo = float(input("Rendimento por período (%): "))
+    quant_aporte =  float(input("Aporte a cada período: R$ "))
+    periodos = int(input("Quantidade total de períodos: "))
+    calcular_juros_compostos(valor_inicial, quant_aporte, rendimento_periodo, periodos)
+aplicar_juros_dados_usuario()

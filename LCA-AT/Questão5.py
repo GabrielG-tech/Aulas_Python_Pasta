0ce3597c0e56ad4500e9220ee46fd76493d8a4f1
Gabriel <gabriel@mpsouza.com.br>
2022-06-24 02:12:56 -0300

    Terminei_o_AT

--- LCA-AT/Questão5.py
+++ LCA-AT/Questão5.py
@@ -11,9 +11,5 @@ print(tabela_pib)
 
 # 3. O código desenvolvido exibiu corretamente o gráfico da evolução do PIB de um país na questão 5?
 
-# 4. O aluno conceitualizou bancos de dados?
 
-# 4. O aluno deu exemplos de uso de bancos de dados em aplicações?
-
-# 3. Falta 9/14
-# 4. Falta 2/6
+# 3. Falta 4/14
